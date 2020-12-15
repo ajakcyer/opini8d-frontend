91cab41505e525c4459d5e60600f63e16ccfd4af
ajakcyer <ajakcyer97@gmail.com>
2020-12-14 19:11:45 -0500

    working on making users see if they already voted for an opinion

--- src/OpinionPage/Opinion.js
+++ src/OpinionPage/Opinion.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { editOpinion, deleteOpinion, opinionRated } from '../Redux/action'
-import {withRouter} from 'react-router-dom'
+import { editOpinion, deleteOpinion, opinionRated } from "../Redux/action";
+import { withRouter } from "react-router-dom";
 
 class Opinion extends Component {
   state = {
@@ -17,14 +17,14 @@ class Opinion extends Component {
         return this.props.ratePost({
           userId: this.props.currentUser,
           id: this.props.opinion.id,
-          agreeable: false
-        })
+          agreeable: false,
+        });
       case "Agree":
         return this.props.ratePost({
           userId: this.props.currentUser,
           id: this.props.opinion.id,
-          agreeable: true
-        })
+          agreeable: true,
+        });
     }
   };
 
@@ -43,63 +43,69 @@ class Opinion extends Component {
     this.props.editPost({
       id: this.props.opinion.id,
       title: this.state.title,
-      content: this.state.content
-    })
+      content: this.state.content,
+    });
     this.setState((prev) => ({ editBtn: false }));
   };
 
-  deleteBtn = () =>{
-    this.props.deletePost(this.props.opinion)
-    this.props.history.goBack()
-  }
+  deleteBtn = () => {
+    this.props.deletePost(this.props.opinion);
+    this.props.history.goBack();
+  };
 
-  ratings = () =>{
+  ratings = () => {
     // debugger
     // let color;
-    if (this.props.opinion.ratings.length > 0){
-      let positive = this.props.opinion.ratings.filter(rating => rating.agreeable === true)
-      let negative = this.props.opinion.ratings.filter(rating => rating.agreeable === false)
+    if (this.props.opinion.ratings.length > 0) {
+      let positive = this.props.opinion.ratings.filter(
+        (rating) => rating.agreeable === true
+      );
+      let negative = this.props.opinion.ratings.filter(
+        (rating) => rating.agreeable === false
+      );
       // debugger
       return (
         <>
-          <h5>{(positive.length / this.props.opinion.ratings.length) * 100}% Agree</h5>
-          <h5>{(negative.length / this.props.opinion.ratings.length) * 100}% disagree</h5>
-          <p>Out of {this.props.opinion.ratings.length} {this.props.opinion.ratings.length > 1 ? "voters" : "voter"}</p>
+          <h5>
+            {(positive.length / this.props.opinion.ratings.length) * 100}% Agree
+          </h5>
+          <h5>
+            {(negative.length / this.props.opinion.ratings.length) * 100}%
+            disagree
+          </h5>
+          <p>
+            Out of {this.props.opinion.ratings.length}{" "}
+            {this.props.opinion.ratings.length > 1 ? "voters" : "voter"}
+          </p>
         </>
-      )
-      // debugger
-      // if (positive.length > negative.length){
-      //   return color = "Green"
-      // } else if (positive.length < negative.length){
-      //   // debugger
-      //   return color = "Red"
-      // } else if (positive.length === negative.length){
-      //   return color = "Grey"
-      // }
+      );
     } else {
-      return(
+      return (
         <>
           <h5>0% Agree</h5>
           <h5>0% Disagree</h5>
           <p>No ratings yet!</p>
         </>
-
-      )
-      // return color = "Grey"
+      );
     }
-    
+  };
+
+  didIVote = () =>{
+    let myVote = this.props.opinion.ratings.find(rating => rating.user_id === this.props.currentUser)
+    return myVote
   }
 
-
   render() {
-    // console.log(this.ratings())
+    console.log("My Vote: ", this.didIVote());
     return (
       <div>
         {this.ratings()}
         <h4>By: {this.props.opinion.user.username}</h4>
         {this.props.currentUser === this.props.opinion.user.id ? (
           <>
-            <button onClick={this.editClicked}>{this.state.editBtn ? "Nevermind" : "Edit Post"}</button>
+            <button onClick={this.editClicked}>
+              {this.state.editBtn ? "Nevermind" : "Edit Post"}
+            </button>
             <button onClick={this.deleteBtn}>Delete Post</button>
           </>
         ) : null}
@@ -125,6 +131,7 @@ class Opinion extends Component {
           <>
             <h2>{this.props.opinion.title}</h2>
             <p>{this.props.opinion.content}</p>
+
             <button onClick={this.onClickHandler}>Agree</button>
             <button onClick={this.onClickHandler}>Disagree</button>
           </>
@@ -142,8 +149,8 @@ const mdp = (dispatch) => {
   return {
     editPost: (opinion) => dispatch(editOpinion(opinion)),
     deletePost: (opinion) => dispatch(deleteOpinion(opinion)),
-    ratePost: (opinion) => dispatch(opinionRated(opinion))
-   };
+    ratePost: (opinion) => dispatch(opinionRated(opinion)),
+  };
 };
 
 export default withRouter(connect(msp, mdp)(Opinion));
