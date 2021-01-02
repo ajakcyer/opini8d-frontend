95cafd90139bacb5a34527fcc2f181bb4a03dd97
ajakcyer <ajakcyer97@gmail.com>
2021-01-02 16:41:09 -0500

    added sticky navbar to left side using css grid

--- src/MainContainer/Main.js
+++ src/MainContainer/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import { NavLink, Route, Switch, withRouter } from "react-router-dom";
 import AllOpinions from "../ExplorePage/AllOpinions";
 import Profile from "../Profile/Profile";
@@ -6,9 +6,20 @@ import { connect } from "react-redux";
 import { fetchCategories, fetchOpinionsFromApi, logoutAction } from "../Redux/action";
 import logo from '../opini8d-logo.png'
 import Followed from '../ExplorePage/Followed';
-import { Menu } from 'semantic-ui-react'
+import { Menu, Ref, Sticky, Rail } from 'semantic-ui-react'
 
 class Main extends Component {
+
+  state = {
+    activeItem: 'Home'
+  }
+
+  contextRef = createRef()
+
+  handleItemClick = (e, {name}) => {
+    this.setState({activeItem: name})
+  }
+
   componentDidMount = () => {
     fetch("http://localhost:3000/api/v1/opinions")
       .then((r) => r.json())
@@ -37,11 +48,61 @@ class Main extends Component {
     // debugger
     console.log(this.props)
     return (
-    <>
-      <h1>Main (logged in) Component Container</h1>
-      <img className="logo" src={logo} alt="logo" />
+      <Ref innerRef={this.contextRef}>
+    <div className="all-content">
+      {/* <h1>Main (logged in) Component Container</h1> */}
+      {/* <img className="logo" src={logo} alt="logo" /> */}
       <br></br>
-      <NavLink to="/explore/opinions">Explore Opinions</NavLink>
+        <Rail>
+      <Sticky context={this.contextRef}>
+      <div className="navbar">
+
+      
+      <Menu  pointing secondary vertical >
+        {/* <Menu.Item header><img className="logo" src={logo} alt="logo" /></Menu.Item> */}
+      <img className="logo" src={logo} alt="logo" />
+        <NavLink to="/explore/home">
+          <Menu.Item
+            name="Home"
+            active={this.state.activeItem === 'Home'}
+            onClick={this.handleItemClick}
+          />
+        </NavLink>
+
+        <NavLink to="/explore/profile">
+          <Menu.Item
+            name="Profile"
+            active={this.state.activeItem === 'Profile'}
+            onClick={this.handleItemClick}
+          />
+        </NavLink>
+
+        <NavLink to="/explore/opinions">
+          <Menu.Item
+            name="Explore"
+            active={this.state.activeItem === 'Explore'}
+            onClick={this.handleItemClick}
+          />
+        </NavLink>
+
+        <Menu.Item
+          name="Log Out"
+          active={this.state.activeItem === 'Log Out'}
+          onClick={(e, {name})=>{
+            this.handleItemClick(e, name)
+            this.props.logout()
+            localStorage.removeItem("token")
+            this.props.history.push('/auth/login')
+            }}
+        />
+
+      </Menu>
+      </div>
+      </Sticky>
+      </Rail>
+      
+
+      {/* <NavLink to="/explore/opinions">Explore Opinions</NavLink>
       <br></br>
       <NavLink to="/explore/profile">View Profile</NavLink>
       <br></br>
@@ -52,8 +113,11 @@ class Main extends Component {
         this.props.logout()
         localStorage.removeItem("token")
         this.props.history.push('/auth/login')
-        }} className="link-button">Log out</button>
+        }} className="link-button">Log out</button> */}
+      <div className="main-content">
+      <h1>Main (logged in) Component Container</h1>
 
+      
       {this.props.opinions.length === 0 ? <h1>Loading...</h1> :
       
       this.props.currentUser ? 
@@ -79,8 +143,10 @@ class Main extends Component {
         : null
 
       }
+      </div>
 
-    </>
+    </div>
+    </Ref>
       
     )
   }
