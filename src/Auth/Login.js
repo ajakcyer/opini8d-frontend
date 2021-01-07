abb4d817a2e16a761d37b863915eb6e6bbd7ba34
ajakcyer <ajakcyer97@gmail.com>
2021-01-07 05:22:06 -0500

    finished styling auth components

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { NavLink, withRouter } from 'react-router-dom'
+import { Button, Form, Input } from 'semantic-ui-react'
 import { loginAction } from '../Redux/action'
 
 class Login extends Component {
@@ -26,15 +27,15 @@ class Login extends Component {
     render() {
         // console.log(this.props)
         return (
-            <div>
-                <h1>Login Component</h1>
-                <form onSubmit={this.onSubmitHandler} >
-                    <input onChange={this.onChangeHandler} type="text" name="username" placeholder="Username" value={this.state.username} />
+            <div className="auth-login-c">
+                <h1>Login</h1>
+                <Form className="auth-login-form" onSubmit={this.onSubmitHandler} >
+                    <Input onChange={this.onChangeHandler} type="text" name="username" placeholder="Username" value={this.state.username} />
                     <br></br>
-                    <input onChange={this.onChangeHandler} type="password" name="password" placeholder="Password" value={this.state.password} />
+                    <Input onChange={this.onChangeHandler} type="password" name="password" placeholder="Password" value={this.state.password} />
                     <br></br>
-                    <button>Log in</button>
-                </form>
+                    <Button color="violet">Log in</Button>
+                </Form>
                 <br></br>
                 <NavLink to="/auth/signup">
                     Create an Account
